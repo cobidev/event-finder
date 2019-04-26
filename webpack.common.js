12ea4ab1308a74794f29cd46e087ea26c6e9bc1f
Jacobo Martinez <jacobojavier98@gmail.com>
2019-04-26 00:42:01 -0400

    Extract CSS and Minify HTML/CSS/JS files. All done in webpack.prod.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,10 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
 module.exports = {
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', //3. Inject styles into DOM
-          'css-loader', //2. Turns css into commonjs
-          'sass-loader'] //1. Turns sass into css
-      },
       {
         test: /\.html$/,
         use: ['html-loader']
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,23 @@ const path = require('path');
 module.exports = merge(common, {
   mode: 'development',
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', //3. Inject styles into DOM
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader'] //1. Turns sass into css
+      },
+    ]
   }
 });
