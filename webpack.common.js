31cbddbc463546071a76731fd847c1161c06c4ef
Jacobo Martinez <jacobojavier98@gmail.com>
2019-04-25 23:32:29 -0400

    add multiple entrypoints in webpack common config and add vendor js to load bootstrap js files

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    vendor: './src/vendor.js'
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html'
@@ -16,6 +19,20 @@ module.exports = {
           'style-loader', //3. Inject styles into DOM
           'css-loader', //2. Turns css into commonjs
           'sass-loader'] //1. Turns sass into css
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs'
+          }
+        }
       }
     ]
   }
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
 const path = require('path');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'main.[contentHash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new cleanWebpackPlugin(),
+  ]
 })
